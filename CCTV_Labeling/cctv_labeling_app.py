481ec27b249284ec85b71e4ee370fdb5132900d7
yusang <sang981113@gmail.com>
2022-09-28 16:43:46 +0900

    image file control

--- CCTV_Labeling/cctv_labeling_app.py
+++ CCTV_Labeling/cctv_labeling_app.py
@@ -1,10 +1,11 @@
+from bdb import checkfuncname
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
 
 import sys
 import os
 
-APP_NAME = 'CCTV_Verification_Test'
+APP_NAME = 'CCTV_Verification'
 
 class LabelingMain(QMainWindow):
     
@@ -12,7 +13,7 @@ class LabelingMain(QMainWindow):
         super().__init__()
         self.folder_path = ""
         self.file_index = 0
-        self.scale = 2
+        self.width_scale = 960
         self.initUI()
 
     def initUI(self):
@@ -23,7 +24,7 @@ class LabelingMain(QMainWindow):
         self.menubar = self.menuBar()
         self.menu_open = self.menubar.addMenu("파일")
         self.menu_open_folder = QAction("폴더 열기", self)
-        self.menu_open_folder.triggered.connect(lambda: self.setImageScaledByIndex((self.scale), self.getFolderPathByOpenDir(), 0))
+        self.menu_open_folder.triggered.connect(lambda: self.setScaledImage((self.width_scale), self.getFolderPathByOpenDir(), 0))
         self.menu_open.addAction(self.menu_open_folder)
 
         self.statusbar = self.statusBar()
@@ -53,11 +54,11 @@ class LabelingMain(QMainWindow):
 
         self.image_layout = QHBoxLayout()
         self.actual_image_label = QLabel()
-        self.actual_image_label.setPixmap(QtGui.QPixmap("test.jpg").scaledToWidth(960))
+        self.actual_image_label.setPixmap(QtGui.QPixmap("screen.png").scaledToWidth(self.width_scale))
         self.actual_image_label.setMargin(3)
         self.image_layout.addWidget(self.actual_image_label)
         self.predict_image_label = QLabel()
-        self.predict_image_label.setPixmap(QtGui.QPixmap("test.jpg").scaledToWidth(960))
+        self.predict_image_label.setPixmap(QtGui.QPixmap("screen.png").scaledToWidth(self.width_scale))
         self.predict_image_label.setMargin(3)
         self.image_layout.addWidget(self.predict_image_label)
         self.main_layout.addLayout(self.image_layout)
@@ -65,12 +66,12 @@ class LabelingMain(QMainWindow):
         self.prev_btn = QPushButton()
         self.prev_btn.setEnabled(False)
         self.prev_btn.setIcon(self.style().standardIcon(QStyle.SP_ArrowLeft))
-        self.prev_btn.clicked.connect(lambda: self.prevBtnAction(self.scale, self.folder_path, self.file_index))
+        self.prev_btn.clicked.connect(lambda: self.prevBtnAction(self.width_scale, self.folder_path, self.file_index))
 
         self.next_btn = QPushButton()
         self.next_btn.setEnabled(False)
         self.next_btn.setIcon(self.style().standardIcon(QStyle.SP_ArrowRight))
-        self.next_btn.clicked.connect(lambda: self.nextBtnAction(self.scale, self.folder_path, self.file_index))
+        self.next_btn.clicked.connect(lambda: self.nextBtnAction(self.width_scale, self.folder_path, self.file_index))
 
         self.control_button_layout = QHBoxLayout()
         self.control_button_layout.addWidget(self.prev_btn)
@@ -83,52 +84,89 @@ class LabelingMain(QMainWindow):
     """
         사진 폴더 열기 기능관련 함수
     """
-    def setImageScaledByIndex(self, scale, folder_path, index):
-        pixmap = QtGui.QPixmap(self.getFilePathByIndex(folder_path, index))
-        pixmap = pixmap.scaled(int(pixmap.width() / scale), int(pixmap.height() / scale))
-        self.actual_image_label.setPixmap(pixmap)
-        self.folder_path = folder_path
-
-    def setActualImage(self):
-    
-    def setPredictImage(self):
-        
-
-    def getFilePathByIndex(self, folder_path, index):
+    def getFolderPathByOpenDir(self):
+        folder_path = str(QFileDialog.getExistingDirectory(self, "이미지 폴더 불러오기"))
         try: 
             file_list = os.listdir(folder_path)
         except FileNotFoundError:
             return
-        file_name_list = []
+        self.predict_file_name_list = self.getFileNameList(True, file_list)
+        self.actual_file_name_list = self.getFileNameList(False, file_list)
 
-        for file in file_list:
-            file_name, file_ext = os.path.splitext(file)
-            if file_ext == '.jpg':
-                file_name_list.append(file_name)
+        self.file_name_list = self.getSameNameList(self.getFileNameList(True, file_list), self.getFileNameList(False, file_list))
+        if len(self.file_name_list) == 0:
+            return
 
-        if index == 0:
+        return folder_path
+
+    def setScaledImage(self, scale, folder_path, index):
+        if folder_path == None:
+            QMessageBox.warning(self, '알림', '사용할 수 없는 폴더입니다.')
+            return
+        self.setImage(scale, folder_path, index, False)
+        self.setImage(scale, folder_path, index, True)
+        self.folder_path = folder_path
+        
+    def setImage(self, scale, folder_path, index, isPredict):
+        pixmap = QtGui.QPixmap(self.getFilePath(folder_path, index, isPredict))
+        pixmap = pixmap.scaledToWidth(scale)
+
+        if index == 0 and self.file_count > 1:
             self.prev_btn.setEnabled(False)
             self.next_btn.setEnabled(True)
-        elif index == len(file_name_list) - 1:
+        elif index == 0 and self.file_count <= 1:
+            self.prev_btn.setEnabled(False)
+            self.next_btn.setEnabled(False)
+        elif index == self.file_count - 1:
             self.prev_btn.setEnabled(True)
             self.next_btn.setEnabled(False)
         else:
             self.prev_btn.setEnabled(True)
             self.next_btn.setEnabled(True)
+
+        if isPredict:
+            self.predict_image_label.setPixmap(pixmap)
+        else:
+            self.actual_image_label.setPixmap(pixmap)
         
-        return folder_path + '/' + file_name_list[index] + '.jpg'
+    def getFilePath(self, folder_path, index, isPredict):
+        self.file_count = len(self.file_name_list)
+        if isPredict:
+            return folder_path + '/res_' + self.file_name_list[index] + '.jpg'
+        else:
+            return folder_path + '/save_' + self.file_name_list[index] + '.jpg'
+
+    def getSameNameList(self, predict_file_name_list, actual_file_name_list):
+        same_name_list = []
+        for i in range(len(predict_file_name_list)):
+            if predict_file_name_list[i].split('_', maxsplit=1)[1] == actual_file_name_list[i].split('_', maxsplit=1)[1]:
+                same_name_list.append(predict_file_name_list[i].split('_', maxsplit=1)[1])
+        return same_name_list
+
+
+    def getFileNameList(self, isPredict, file_list):
+        file_name_list = []
+        for file in file_list:
+            file_name, file_ext = os.path.splitext(file)
+            if file_ext == '.jpg' and not isPredict:
+                if not file_name.startswith('save'):
+                    continue
+                file_name_list.append(file_name)
+            elif file_ext == '.jpg' and isPredict:
+                if not file_name.startswith('res'):
+                    continue
+                file_name_list.append(file_name)
+
+        return file_name_list
 
     def prevBtnAction(self, scale, folder_path, index):
-        self.setImageScaledByIndex(scale, folder_path, index-1)
+        self.setScaledImage(scale, folder_path, index-1)
         self.file_index = index - 1
 
     def nextBtnAction(self, scale, folder_path, index):
-        self.setImageScaledByIndex(scale, folder_path, index+1)
+        self.setScaledImage(scale, folder_path, index+1)
         self.file_index = index + 1
 
-
-    def getFolderPathByOpenDir(self):
-        return str(QFileDialog.getExistingDirectory(self, "사진 폴더 불러오기"))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
