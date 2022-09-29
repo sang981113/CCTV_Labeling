9b0356812c4aec53ee452334e71e0d62a5cdc4c1
yusang <sang981113@gmail.com>
2022-09-29 10:30:51 +0900

    add json logic

--- CCTV_Labeling/cctv_labeling_app.py
+++ CCTV_Labeling/cctv_labeling_app.py
@@ -6,6 +6,10 @@ import sys
 import os
 
 APP_NAME = 'CCTV_Verification'
+ACTUAL_OFFSET = 'save'
+PREDICT_OFFSET = 'res'
+IMAGE_EXT_OFFSET = '.jpg'
+DATA_EXT_OFFSET = '.json'
 
 class LabelingMain(QMainWindow):
     
@@ -25,6 +29,8 @@ class LabelingMain(QMainWindow):
         self.menu_open = self.menubar.addMenu("파일")
         self.menu_open_folder = QAction("폴더 열기", self)
         self.menu_open_folder.triggered.connect(lambda: self.setScaledImage((self.width_scale), self.getFolderPathByOpenDir(), 0))
+        self.log_open = self.menubar.addMenu("로그")
+        self.log_open_folder = QAction("로그 보기", self)
         self.menu_open.addAction(self.menu_open_folder)
 
         self.statusbar = self.statusBar()
@@ -51,6 +57,55 @@ class LabelingMain(QMainWindow):
         self.main_groupbox = QGroupBox()
         self.setCentralWidget(self.main_groupbox)
         self.main_layout = QVBoxLayout(self.main_groupbox)
+
+        self.top_text_data_layout = QHBoxLayout()
+        self.main_layout.addLayout(self.top_text_data_layout)
+
+        self.json_data_layout = QVBoxLayout()
+        self.actual_people_count_label = QLabel("실제 사람 수: ")
+        self.actual_people_count_label.setFont(QtGui.QFont('Arial', 20))
+        self.actual_people_count_value_label = QLabel()
+        self.actual_dumping_yn_label = QLabel("실제 투기 여부: ")
+        self.actual_dumping_yn_label.setFont(QtGui.QFont('Arial', 20))
+        self.actual_dumping_yn_value_label = QLabel()
+        self.predict_people_count_label = QLabel("예측 사람 수: ")
+        self.predict_people_count_label.setFont(QtGui.QFont('Arial', 20))
+        self.predict_people_count_value_label = QLabel()
+        self.predict_dumping_yn_label = QLabel("예측 투기 여부: ")
+        self.predict_dumping_yn_label.setFont(QtGui.QFont('Arial', 20))
+        self.predict_dumping_yn_value_label = QLabel()
+        self.json_data_layout.addWidget(self.actual_people_count_label)
+        self.json_data_layout.addWidget(self.actual_people_count_value_label)
+        self.json_data_layout.addWidget(self.actual_dumping_yn_label)
+        self.json_data_layout.addWidget(self.actual_dumping_yn_value_label)
+        self.json_data_layout.addWidget(self.predict_people_count_label)
+        self.json_data_layout.addWidget(self.predict_people_count_value_label)
+        self.json_data_layout.addWidget(self.predict_dumping_yn_label)
+        self.json_data_layout.addWidget(self.predict_dumping_yn_value_label)
+        self.json_data_layout.setAlignment(QtCore.Qt.AlignLeft)
+        self.top_text_data_layout.addLayout(self.json_data_layout)
+
+        self.confusion_matrix_layout = QGridLayout()
+        self.confusion_matrix_layout.rowStretch(0)
+        self.confusion_matrix_layout.columnStretch(0)
+        self.true_positive_label = QLabel('True Positive: ')
+        self.true_positive_label.setFont(QtGui.QFont('Arial', 20))
+        self.true_positive_value_label = QLabel()
+        self.false_negative_label = QLabel('False Negative: ')
+        self.false_negative_label.setFont(QtGui.QFont('Arial', 20))
+        self.false_negative_value_label = QLabel()
+        self.false_positive_label = QLabel('False Positive: ')
+        self.false_positive_label.setFont(QtGui.QFont('Arial', 20))
+        self.false_positive_value_label = QLabel()
+        self.true_negative_label = QLabel('True Negative: ')
+        self.true_negative_label.setFont(QtGui.QFont('Arial', 20))
+        self.true_negative_value_label = QLabel()
+        self.confusion_matrix_layout.addWidget(self.true_positive_label, 0, 0, 1, 1)
+        self.confusion_matrix_layout.addWidget(self.false_negative_label, 0, 1, 1, 1)
+        self.confusion_matrix_layout.addWidget(self.false_positive_label, 1, 0, 1, 1)
+        self.confusion_matrix_layout.addWidget(self.true_negative_label, 1, 1, 1, 1)
+        self.confusion_matrix_layout.setAlignment(QtCore.Qt.AlignCenter)
+        self.top_text_data_layout.addLayout(self.confusion_matrix_layout)
 
         self.image_layout = QHBoxLayout()
         self.actual_image_label = QLabel()
@@ -90,11 +145,12 @@ class LabelingMain(QMainWindow):
             file_list = os.listdir(folder_path)
         except FileNotFoundError:
             return
-        self.predict_file_name_list = self.getFileNameList(True, file_list)
-        self.actual_file_name_list = self.getFileNameList(False, file_list)
 
-        self.file_name_list = self.getSameNameList(self.getFileNameList(True, file_list), self.getFileNameList(False, file_list))
-        if len(self.file_name_list) == 0:
+        self.predict_image_name_list = self.getSameNameList(self.getFileNameList(True, IMAGE_EXT_OFFSET, file_list), self.getFileNameList(True, DATA_EXT_OFFSET, file_list))
+        self.actual_image_name_list = self.getFileNameList(False, IMAGE_EXT_OFFSET, file_list)
+        self.image_name_list = self.getMatchedImageList(self.predict_image_name_list, self.actual_image_name_list)
+        self.file_count = len(self.image_name_list)
+        if len(self.image_name_list) == 0:
             return
 
         return folder_path
@@ -105,11 +161,6 @@ class LabelingMain(QMainWindow):
             return
         self.setImage(scale, folder_path, index, False)
         self.setImage(scale, folder_path, index, True)
-        self.folder_path = folder_path
-        
-    def setImage(self, scale, folder_path, index, isPredict):
-        pixmap = QtGui.QPixmap(self.getFilePath(folder_path, index, isPredict))
-        pixmap = pixmap.scaledToWidth(scale)
 
         if index == 0 and self.file_count > 1:
             self.prev_btn.setEnabled(False)
@@ -124,19 +175,33 @@ class LabelingMain(QMainWindow):
             self.prev_btn.setEnabled(True)
             self.next_btn.setEnabled(True)
 
+        self.lbl_folder_path.show()
+        self.lbl_folder_path_value.setText(folder_path)
+        self.lbl_folder_path_value.show()
+        self.lbl_file_name.show()
+        self.lbl_file_name_value.setText(ACTUAL_OFFSET + '_' + self.image_name_list[index] + IMAGE_EXT_OFFSET + ', ' + PREDICT_OFFSET + '_' + self.image_name_list[index] + IMAGE_EXT_OFFSET)
+        self.lbl_file_name_value.show()
+        self.lbl_index.show()
+        self.lbl_index_value.setText(str(index+1))
+        self.lbl_index_value.show()
+        self.folder_path = folder_path
+        
+    def setImage(self, scale, folder_path, index, isPredict):
+        pixmap = QtGui.QPixmap(self.getFilePath(folder_path, index, isPredict))
+        pixmap = pixmap.scaledToWidth(scale)
+
         if isPredict:
             self.predict_image_label.setPixmap(pixmap)
         else:
             self.actual_image_label.setPixmap(pixmap)
         
     def getFilePath(self, folder_path, index, isPredict):
-        self.file_count = len(self.file_name_list)
         if isPredict:
-            return folder_path + '/res_' + self.file_name_list[index] + '.jpg'
+            return folder_path + '/' + PREDICT_OFFSET + '_' + self.image_name_list[index] + IMAGE_EXT_OFFSET
         else:
-            return folder_path + '/save_' + self.file_name_list[index] + '.jpg'
+            return folder_path + '/' + ACTUAL_OFFSET + '_' + self.image_name_list[index] + IMAGE_EXT_OFFSET
 
-    def getSameNameList(self, predict_file_name_list, actual_file_name_list):
+    def getMatchedImageList(self, predict_file_name_list, actual_file_name_list):
         same_name_list = []
         for i in range(len(predict_file_name_list)):
             if predict_file_name_list[i].split('_', maxsplit=1)[1] == actual_file_name_list[i].split('_', maxsplit=1)[1]:
@@ -144,20 +209,38 @@ class LabelingMain(QMainWindow):
         return same_name_list
 
 
-    def getFileNameList(self, isPredict, file_list):
+    def getFileNameList(self, isPredict, file_ext_offset, file_list):
         file_name_list = []
         for file in file_list:
             file_name, file_ext = os.path.splitext(file)
-            if file_ext == '.jpg' and not isPredict:
-                if not file_name.startswith('save'):
+            if file_ext == file_ext_offset and not isPredict:
+                if not file_name.startswith(ACTUAL_OFFSET):
                     continue
                 file_name_list.append(file_name)
-            elif file_ext == '.jpg' and isPredict:
-                if not file_name.startswith('res'):
+            elif file_ext == file_ext_offset and isPredict:
+                if not file_name.startswith(PREDICT_OFFSET):
+                    continue
+                file_name_list.append(file_name)
+            elif file_ext == file_ext_offset and isPredict:
+                if not file_name.startswith(PREDICT_OFFSET):
                     continue
                 file_name_list.append(file_name)
 
         return file_name_list
+
+    def getSameNameList(self, file_list1, file_list2):
+        name_list1 = []
+        name_list2 = []
+
+        for file in file_list1:
+            file_name, _ = os.path.splitext(file)
+            name_list1.append(file_name)
+        
+        for file in file_list2:
+            file_name, _ = os.path.splitext(file)
+            name_list2.append(file_name)
+        
+        return list(set(name_list1) & set(name_list2))
 
     def prevBtnAction(self, scale, folder_path, index):
         self.setScaledImage(scale, folder_path, index-1)
